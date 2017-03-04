d1dd759e7de5743a65775dd066f9070ddfb99777
小胖 <yhz@xiaopangdeMacBookPro.local>
2017-03-04 11:36:15 +0800

    init

---
+++ mylibrary/src/main/java/com/yuhuazhu/mylibrary/MyUtils.java
@@ -0,0 +1,14 @@
+package com.yuhuazhu.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by yhz on 2017/3/4.
+ *
+ */
+
+public class MyUtils {
+    static void log() {
+        Log.e("123", "abc");
+    }
+}
